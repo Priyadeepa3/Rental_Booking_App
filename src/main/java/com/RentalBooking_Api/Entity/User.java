0063c9912aff956ba37950b3a5b90609afe36f62
Priyadharshini Thirugnanasambandam <priyathiru333@gmail.com>
2026-06-15 13:04:54 +0530

    Update User.java

--- src/main/java/com/RentalBooking_Api/Entity/User.java
+++ src/main/java/com/RentalBooking_Api/Entity/User.java
@@ -37,23 +37,17 @@ public class User {
         this.createdAt = LocalDateTime.now();
     }
 
-    // Getters & Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
-
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-
     public LocalDateTime getCreatedAt() { return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
 
-    // equals & hashCode (based on id only)
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -63,11 +57,8 @@ public class User {
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+    public int hashCode() { return Objects.hash(id); }
 
-    // Safer toString (exclude password)
     @Override
     public String toString() {
         return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
